f01bcb9aa346e56f00b38fae80ab13e5c19010df
javierpb275 <javierpb275@gmail.com>
2022-07-25 19:17:18 +0200

    installed express, created new cloud function using express and set env variables with command and redeployed functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,8 @@
 import * as functions from "firebase-functions";
+import * as express from "express";
 
-// Start writing Firebase Functions
-// https://firebase.google.com/docs/functions/typescript
+const app = express();
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-});
+app.get("/", (req, res) => res.status(200).send("Hey there!"));
+
+exports.app = functions.https.onRequest(app);
