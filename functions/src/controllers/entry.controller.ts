9dae81eebac3c858465662f2fa7f8901beea7825
javierpb275 <javierpb275@gmail.com>
2022-07-25 19:54:33 +0200

    set firebase configuration and created entry controller with cloud function addEntry and added new route post /entries. added data to firestore successfully

---
+++ functions/src/controllers/entry.controller.ts
@@ -0,0 +1,37 @@
+import { Response } from "express";
+import { db } from "../config/firebase";
+
+type EntryType = {
+  title: string;
+  text: string;
+  coverImageUrl: string;
+};
+
+type Request = {
+  body: EntryType;
+  params: { entryId: string };
+};
+
+const addEntry = async (req: Request, res: Response) => {
+  const { title, text } = req.body;
+  try {
+    const entry = db.collection("entries").doc();
+    const entryObject = {
+      id: entry.id,
+      title,
+      text,
+    };
+
+    entry.set(entryObject);
+
+    res.status(200).send({
+      status: "success",
+      message: "entry added successfully",
+      data: entryObject,
+    });
+  } catch (error: any) {
+    res.status(500).json(error.message);
+  }
+};
+
+export { addEntry }
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,10 @@
 import * as functions from "firebase-functions";
 import * as express from "express";
+import { addEntry } from "./controllers/entry.controller";
 
 const app = express();
 
 app.get("/", (req, res) => res.status(200).send("Hey there!"));
+app.post("/entries", addEntry);
 
 exports.app = functions.https.onRequest(app);
