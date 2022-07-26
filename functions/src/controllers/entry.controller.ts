be95170f52737d66d09b4b233335699ab557aed5
javierpb275 <javierpb275@gmail.com>
2022-07-26 18:25:05 +0200

    created update and delete entry cloud functions

--- functions/src/controllers/entry.controller.ts
+++ functions/src/controllers/entry.controller.ts
@@ -45,4 +45,58 @@ const getAllEntries = async (req: Request, res: Response) => {
   }
 };
 
-export { addEntry, getAllEntries };
+const updateEntry = async (req: Request, res: Response) => {
+  const {
+    body: { text, title },
+    params: { entryId },
+  } = req;
+
+  try {
+    const entry = db.collection("entries").doc(entryId);
+    const currentData = (await entry.get()).data() || {};
+    const entryObject = {
+      id: entryId,
+      title: title || currentData.title,
+      text: text || currentData.text,
+    };
+
+    await entry.set(entryObject).catch((error) => {
+      return res.status(400).json({
+        status: "error",
+        message: error.message,
+      });
+    });
+
+    return res.status(200).json({
+      status: "success",
+      message: "entry updated successfully",
+      data: entryObject,
+    });
+  } catch (error: any) {
+    return res.status(500).json(error.message);
+  }
+};
+
+const deleteEntry = async (req: Request, res: Response) => {
+  const { entryId } = req.params;
+
+  try {
+    const entry = db.collection("entries").doc(entryId);
+
+    await entry.delete().catch((error) => {
+      return res.status(400).json({
+        status: "error",
+        message: error.message,
+      });
+    });
+
+    return res.status(200).json({
+      status: "success",
+      message: "entry deleted successfully",
+    });
+  } catch (error: any) {
+    return res.status(500).json(error.message);
+  }
+};
+
+export { addEntry, getAllEntries, updateEntry, deleteEntry };
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,11 +1,18 @@
 import * as functions from "firebase-functions";
 import * as express from "express";
-import { addEntry, getAllEntries } from "./controllers/entry.controller";
+import {
+  addEntry,
+  getAllEntries,
+  updateEntry,
+  deleteEntry,
+} from "./controllers/entry.controller";
 
 const app = express();
 
 app.get("/", (req, res) => res.status(200).send("Hey there!"));
 app.post("/entries", addEntry);
-app.get('/entries', getAllEntries)
+app.get("/entries", getAllEntries);
+app.patch("/entries/:entryId", updateEntry);
+app.delete("/entries/:entryId", deleteEntry);
 
 exports.app = functions.https.onRequest(app);
