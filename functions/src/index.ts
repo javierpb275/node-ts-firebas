b4391fcefb63b0525d5f654d92f91d9bfa20b1fb
javierpb275 <javierpb275@gmail.com>
2022-07-29 22:25:22 +0200

    created auth middleware and added authorization to routes

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,6 +7,7 @@ import {
   deleteUser,
   createUser,
 } from "./controllers/user.controller";
+import { validateFirebaseIdToken } from "./middlewares/auth.middleware";
 
 const app = express();
 
@@ -14,10 +15,10 @@ app.use(express.json());
 
 app.get("/", (req, res) => res.status(200).send("Hey there!"));
 
-app.get("/users", getUsers);
-app.get("/users/:id", getUserById);
-app.post("/users", createUser);
-app.patch("/users/:id", updateUser);
-app.delete("/users/:id", deleteUser);
+app.get("/users", validateFirebaseIdToken, getUsers);
+app.get("/users/:id", validateFirebaseIdToken, getUserById);
+app.post("/users", validateFirebaseIdToken, createUser);
+app.patch("/users/:id", validateFirebaseIdToken, updateUser);
+app.delete("/users/:id", validateFirebaseIdToken, deleteUser);
 
 exports.app = functions.https.onRequest(app);
